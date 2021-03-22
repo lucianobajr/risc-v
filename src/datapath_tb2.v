bdeedc5cbf4bb01728c16be96707092f95cd9acf
Artur Papa <artur.papa@ufv.br>
2021-03-22 19:18:41 -0300

    2

--- src/datapath_tb2.v
+++ src/datapath_tb2.v
@@ -3,19 +3,19 @@
 module datapath_tb ();
     reg clock, reset;
 
-    main2 Datapath(.clock(clock),.reset(reset));
+    //main2 Datapath(.clock(clock),.reset(reset), );
 
 	always begin
-		clock = 1;#1;
-		clock = 0;#1;
+		clock = 1; #1;
+		clock = 0; #1;
 	end
 
 	initial begin
         $dumpfile("datapath.vcd");
-        $dumpvars(0, datapath_tb);
+        $dumpvars(0, main2);
         
-        $display("Exibindo os resultados:");
-       // $monitor("Instruction: %b\nExit PC: %b\nExit ALU: %b\n", );
+        //$display("Exibindo os resultados:");
+        //$monitor("Instruction: %b\nExit PC: %b\nExit ALU: %b\n", );
 		reset<=1; #3;
 		reset<=0; #1;
 	end
